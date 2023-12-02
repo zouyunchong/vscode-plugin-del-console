8cfa28150eec772a482bb853cac6188965d91d00
zouyunchong <996310793@qq.com>
2023-12-02 13:48:19 +0800

    vscode plugin del console

---
+++ README.md
@@ -0,0 +1,16 @@
+# del-console README
+
+use this plugin, you can del console in your project
+
+## del console in file
+### use command 
+    del-console-file
+### use shortcut key  
+    win：cmd+shift+f
+    mac：cmd+shift+f
+
+## del console in floder(src/*)
+### use command 
+    del-console-folder
+### use shortcut key  
+    win：cmd+shift+m
---
+++ src/extension.ts
@@ -0,0 +1,67 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+let count = 0;
+let hasConsoleLogs = false;
+export function activate(context: vscode.ExtensionContext) {
+	let disposableFolder = vscode.commands.registerCommand('del-console-folder', () => {
+		const workspaceFolders = vscode.workspace.workspaceFolders;
+		if (workspaceFolders) {
+			// 获取第一个工作区的根路径
+			const rootPath = workspaceFolders[0].uri.fsPath;
+			// 拼接路径
+			const srcPath = rootPath + '/src';
+			deleteConsoleLogs(srcPath);
+			context.subscriptions.push(disposableFolder);
+			vscode.window.showInformationMessage(`已删除 ${count} 个 console.log 语句`);
+		} else {
+			console.error('没有打开的工作区');
+		}
+	});
+
+	let disposableFile = vscode.commands.registerCommand('del-console-file', () => {
+		const workspaceFolders = vscode.workspace.workspaceFolders;
+		if (workspaceFolders) {
+			// 获取当前文件的路径
+			const filePath = vscode.window.activeTextEditor?.document.uri.fsPath || '';
+			if (filePath) {
+				deleteConsoleLogsInFile(filePath);
+				context.subscriptions.push(disposableFile);
+				vscode.window.showInformationMessage(`已删除 ${count} 个 console.log 语句`);
+			} else {
+				vscode.window.showInformationMessage('没有文件被打开');
+			}
+		} else {
+			console.error('没有打开的工作区');
+		}
+	});
+
+}
+
+function deleteConsoleLogs(directory: string) {
+	const files = fs.readdirSync(directory);
+	files.forEach((file) => {
+		const filePath = path.join(directory, file);
+		const stats = fs.statSync(filePath);
+		if (stats.isDirectory()) {
+			deleteConsoleLogs(filePath);
+		} else if (stats.isFile()) {
+			deleteConsoleLogsInFile(filePath);
+		}
+	});
+}
+
+function deleteConsoleLogsInFile(filePath: string) {
+	let fileContent = fs.readFileSync(filePath, 'utf-8');
+	const updatedContent = fileContent.replace(/console\.log\(.*?\);/g, '');
+	count += (fileContent.match(/console\.log\(.*?\);/g) || []).length;
+	if (fileContent !== updatedContent) {
+		fs.writeFileSync(filePath, updatedContent, 'utf-8');
+		hasConsoleLogs = true;
+	}
+}
+
+
+export function deactivate() {
+	// TODO: 释放资源和取消注册事件监听器等操作
+}
